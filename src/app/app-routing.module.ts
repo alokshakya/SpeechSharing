566f1ced7df6d9e5d464c25addf423aaf7463dc5
Alok Shakya <alokshakyacode@gmail.com>
2019-03-08 16:50:53 +0530

    home and speech pages loading at same place

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
 const routes: Routes = [
+  {path:'home',component:HomeComponent},
   {
     path: 'speeches',
     loadChildren: './speeches/speeches.module#SpeechesModule'
   },
   {
     path: '',
-    redirectTo: '',
+    redirectTo:'home',
     pathMatch: 'full'
   }
 ];
