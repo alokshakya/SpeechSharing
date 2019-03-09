878be9b97cb24e99a070630a499d6e7c335b5714
Alok Shakya <alokshakyacode@gmail.com>
2019-03-09 20:40:40 +0530

    loading and editing data in my speeches is done

--- src/app/speeches/my-speeches/my-speeches.component.ts
+++ src/app/speeches/my-speeches/my-speeches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-my-speeches',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MySpeechesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+  }
+
+  speechData = {
+    id:2,
+    authorId:2,
+    authorName:'Alok',
+    keywords:['rally', 'election'],
+    text:'This is testing text speech 2',
+    createdDate:"2019-03-10T08:38:22.410Z",
+    updatedDate:"2019-03-11T08:39:10.823Z"
+  };
+  loading:boolean=false;
+  addMessage(){
+    this.loading=true;
+    this.dataService.addSpeech(this.speechData)
+    .subscribe( (res) => {
+      this.loading=false;
+      console.log('response in console ');
+      console.log(res);
+    })
   }
 
 }
