e5b9b62a9646da177b6c213482be9d25a0f2cdbb
Alok Shakya <alokshakyacode@gmail.com>
2019-03-11 01:01:57 +0530

    data service logic added

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -23,9 +23,31 @@ export class DataService {
   speechesArray:Speech[];
   constructor() { }
   getAllSpeeches(): Observable<Speech[]> {
-    // TODO: send the message _after_ fetching the heroes
-    //this.messageService.add('HeroService: fetched heroes');
-    return of(Speeches);
+    this.speechesArray = JSON.parse(localStorage.getItem('speeches'));
+    // of operator returns array and pipe delay returns result after 3 sec to simulate http response
+    return of(this.speechesArray).pipe( delay(3000)); 
+  }
+  getMySpeechesIds(id:number): Observable<number[]> {
+    this.speechesArray = JSON.parse(localStorage.getItem('speeches'));
+    //use filter to filter only speeches of particular user with id
+    var mySpeeches = this.speechesArray.filter(val => {
+      return val.authorId = id;
+    })
+    //return only ids of my speechs so use map
+    var ids = mySpeeches.map( val => {
+      return val.id;
+    })
+    // of operator returns array and pipe delay returns result after 3 sec to simulate http response
+    return of(ids).pipe( delay(3000)); 
+  }
+  getSpeech(id:number):Observable<Speech>{
+    this.speechesArray = JSON.parse(localStorage.getItem('speeches'));
+    //use filter to filter speeches with particular speech id
+    var Speech= this.speechesArray.filter(val => {
+      return val.id = id;
+    });
+    // return 0 index element of filtered array
+    return of(Speech[0]).pipe( delay(3000));
   }
   addSpeech(data:Speech): Observable<Speech[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -42,6 +64,27 @@ export class DataService {
       localStorage.setItem('speeches', JSON.stringify(this.speechesArray));
     }
     
-    return of(this.speechesArray).pipe ( delay( 3000 ) )
+    return of(this.speechesArray).pipe ( delay( 3000 ) );
+  }
+  editSpeech(id:number,newText:string):Observable<Speech>{
+    this.speechesArray = JSON.parse(localStorage.getItem('speeches'));
+    var updated:boolean = false;
+    var ind=0;
+    for(let i=0; i<this.speechesArray.length; i++){
+      if(this.speechesArray[i].id == id){
+        this.speechesArray[i].text = newText;
+        var dateObj = new Date();
+        this.speechesArray[i].updatedDate = dateObj.toISOString();
+        updated=true;
+        ind=i;
+      }
+    }
+    if(updated){
+      localStorage.setItem('speeches', JSON.stringify(this.speechesArray));
+      return of(this.speechesArray[ind]).pipe ( delay( 3000 ) );
+    }
+    else{
+      //not found and not updated error
+    }
   }
 }
