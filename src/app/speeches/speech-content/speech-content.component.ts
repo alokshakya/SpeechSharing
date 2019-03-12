c1806661099484ba1f6a8a9a6c5f7606f4d789a9
Alok Shakya <alokshakyacode@gmail.com>
2019-03-12 16:03:54 +0530

    speech Content Component modified for input and output function

--- src/app/speeches/speech-content/speech-content.component.ts
+++ src/app/speeches/speech-content/speech-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Observable, Subject, ReplaySubject } from 'rxjs';
 import { Speech } from '../../shared/speechInterface';
 import { DataService } from '../../services/data.service';
@@ -11,39 +12,98 @@ import { takeUntil } from 'rxjs/internal/operators';
 })
 export class SpeechContentComponent implements OnInit {
 
-  constructor(private dataService: DataService) { }
+  constructor(private fb: FormBuilder,private dataService: DataService) { }
   @Input('speechId') id: number;
   @Input() events: Observable<void>;
+  @Output() editedSpeech = new EventEmitter<Speech>();
   speechText:string='this is testing test in component ';
   eventsSubscription:any;
   speech:Speech;
   loadingText:boolean=false;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   ngOnInit() {
+    this.initSpeechForm();
     console.log('id of input '+this.id);
+    this.loadInitialSpeech();
+    //subscribe to id change in parent to fetch updated speech
+    this.loadSpeechOnIdChange();
+
+  }
+
+  speechForm:FormGroup;
+  initSpeechForm() {
+    this.speechForm = this.fb.group({
+      text: ['',[]],
+      keywords: ['',[]],
+      authorName: ['', []]
+    });
+    this.speechForm.get('text').valueChanges.subscribe(val => {
+      //emit event
+    });
+    this.speechForm.get('keywords').valueChanges.subscribe(val => {
+      // emit event
+    });
+    this.speechForm.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe( (val) => {
+      this.speech.text = val.text;
+      this.speech.keywords = val.keywords;
+      this.speech.authorName = val.authorName;
+      //send Speech To parent componet
+      this.editedSpeech.emit(this.speech);
+    })
+  }
+
+  loadInitialSpeech(){
     this.loadingText = true;
     this.dataService.getSpeech(this.id).pipe(takeUntil(this.destroyed$)).subscribe( (res) => {
       this.loadingText = false;
       this.speech = res;
-      console.log('speech res ');
-      console.log(res);
+      this.setFormControlValues();
+      //send Speech To parent componet
+      this.editedSpeech.emit(this.speech);
     });
 
+  }
+
+  loadSpeechOnIdChange(){
     this.events.pipe(takeUntil(this.destroyed$)).subscribe(() => {
-      console.log('id clicked in parent displayed in child component by input '+ this.id);
+      
       this.loadingText = true;
       this.dataService.getSpeech(this.id).pipe(takeUntil(this.destroyed$)).subscribe( (res) => {
         this.loadingText = false;
         this.speech = res;
-        console.log('speech res ');
-        console.log(res);
+        this.setFormControlValues();
+        //send Speech To parent componet
+        this.editedSpeech.emit(this.speech);
       });
     });
+  }
+
+  setFormControlValues(){
+    this.speechForm.controls['text'].setValue(this.speech.text);
+    this.speechForm.controls['keywords'].setValue(this.speech.keywords);
+    this.speechForm.controls['authorName'].setValue(this.speech.authorName);
   }
 
   ngOnDestroy(){
     this.destroyed$.next(true);
     this.destroyed$.complete();
+  }
+
+  savingSpeech:boolean=false;
+  save(){
+    this.savingSpeech = true;
+    console.log('speech Content');
+    var dateObj = new Date();
+    this.speech.updatedDate = dateObj.toISOString();
+    console.log(this.speech);
+    this.dataService.editSpeech(this.speech).pipe(takeUntil(this.destroyed$))
+    .subscribe( (res) => {
+      this.savingSpeech = false;
+      this.speech = res;
+    },
+    (err) => {
+      //error handling part
+    })
   }
 
 }
